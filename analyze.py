cb74d19b51ff2797b7a337320bc8e793f7bca103
canisli <canis.li@outlook.com>
2022-06-13 20:19:28 -0700

    calc r^2

--- analyze.py
+++ analyze.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+import csv
 import glob
+import os
 
 ################################
 # Usage: python3 analyze.py
@@ -20,15 +22,21 @@ def exponential(x, A, B,C):
 pause_times = [0.5,1, 1.5, 3, 8]
 index = 0
 
-data_dir = "./data/"
+script_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = script_dir + "/data/"
+
+output = csv.writer(open(script_dir + "/analyze.csv", 'w+', newline=''))
+output.writerow(['name', 'A', 'B', 'C', 'r_squared'])
+
 files = glob.glob(data_dir + "*.TXT")
 files.sort()
 for file in files:
     with open(file, "r") as fr:
         lines = fr.readlines()
+        file_name = file[file.rfind("/") + 1 :]
         print(
             "\n==================  "
-            + file[file.rfind("/") + 1 :] # name of the text file
+            +  file_name # name of the text file
             + "  =================="
         )
 
@@ -48,22 +56,24 @@ for file in files:
         x.pop(0) # Ignore first element since is always zero
         data.pop(0)
 
-        # plt.scatter(x, data)
-        # plt.ylabel('Current reading (mA)')
-        # plt.xlabel('Time after turning on electrolysis (s)')
-        # plt.show()
-
         ## Fit graph to exponential
         parameters, covariance = curve_fit(exponential, x, data)
         A = parameters[0]
         B = parameters[1]
         C = parameters[2]
         fit = exponential(np.array(x), A, B,C)
+        ## Calculate stats
+        residuals = data - fit
+        ss_res = np.sum(residuals**2)
+        ss_tot = np.sum((data-np.mean(data))**2)
+        r_squared = 1 - (ss_res / ss_tot)
 
         print("Fitted values for exponential y=A exp (-Bx) + C:")
         print("A: " + str(A))
         print("B: " + str(B))
         print("C: " + str(C))
+        print("R^2" + str(r_squared))
+        output.writerow([file_name, A, B, C, r_squared])
         
         plt.plot(x, data, 'o', label='data')
         plt.plot(x, fit, '-', label='fit: y= ' +str(round(A,1))+' exp(-'+str(round(B,1))+'x) + '+str(round(C,1)))
@@ -71,7 +81,7 @@ for file in files:
         plt.ylabel('Current reading (mA)')
         plt.xlabel('Time after turning on electrolysis (s)')
         plt.show()
-
+output.close()
 
 
 
