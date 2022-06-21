2dfaa2f4646cd911200fd7c96d23705edafb8e39
canisli <canis.li@outlook.com>
2022-06-20 22:11:51 -0700

    add error bars and uncertainty for params

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 from scipy.optimize import curve_fit
 
 import csv
@@ -26,7 +27,7 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = script_dir + "/data/"
 
 output = csv.writer(open(script_dir + "/analyze.csv", 'w+', newline=''))
-output.writerow(['name', 'A', 'B', 'C', 'r_squared'])
+output.writerow(['name', 'A', 'B', 'C', 'A_err', 'B_err', 'C_err', 'r_squared'])
 
 files = glob.glob(data_dir + "*.TXT")
 files.sort()
@@ -55,13 +56,22 @@ for file in files:
 
         x.pop(0) # Ignore first element since is always zero
         data.pop(0)
+        data_err = np.zeros(len(data)) + 0.05
+        print(data_err)
+        
 
         ## Fit graph to exponential
-        parameters, covariance = curve_fit(exponential, x, data)
+        parameters, covariance = curve_fit(exponential, x, data, sigma=data_err, absolute_sigma=True)
         A = parameters[0]
         B = parameters[1]
         C = parameters[2]
+        parameter_error = np.sqrt(np.diag(covariance)) # https://micropore.wordpress.com/2017/02/04/python-fit-with-error-on-y-axis/
+        A_err = parameter_error[0]
+        B_err = parameter_error[1]
+        C_err = parameter_error[2]
         fit = exponential(np.array(x), A, B,C)
+        print(parameters)
+        print(covariance)
         ## Calculate stats
         residuals = data - fit
         ss_res = np.sum(residuals**2)
@@ -69,14 +79,18 @@ for file in files:
         r_squared = 1 - (ss_res / ss_tot)
 
         print("Fitted values for exponential y=A exp (-Bx) + C:")
-        print("A: " + str(A))
-        print("B: " + str(B))
-        print("C: " + str(C))
-        print("R^2" + str(r_squared))
-        output.writerow([file_name, A, B, C, r_squared] + data)
+        print("Using σ = 0.05 for all the data")
+        print("A: " + str(A), '+ or -', str(A_err))
+        print("B: " + str(B), '+ or -', str(B_err))
+        print("C: " + str(C), '+ or -', str(C_err))  
+        print("R^2=" + str(r_squared))
+        output.writerow([file_name, A, B, C, A_err, B_err, C_err, r_squared] + data)
         
-        plt.plot(x, data, 'o', label='data')
+        rc('font',**{'family':'serif','serif':['Times'],'size':24})
+        #rc('text', usetex=True)
+        plt.errorbar(x, data, fmt='o', yerr = data_err, label='data')
         plt.plot(x, fit, '-', label='fit: y= ' +str(round(A,1))+' exp(-'+str(round(B,1))+'x) + '+str(round(C,1)))
+
         plt.legend()
         plt.ylabel('Current reading (mA)')
         plt.xlabel('Time after turning on electrolysis (s)')
